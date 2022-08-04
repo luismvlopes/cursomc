1d787658671c466f76afbefc245bef750aa34d5e
Luis Lopes <luismvlopes@gmail.com>
2022-08-04 13:38:50 +0100

    ajustes no endpoint /categorias/{id} e lidar com excepcao

--- src/main/java/com/luislopes/cursomc/services/CategoriaService.java
+++ src/main/java/com/luislopes/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.luislopes.cursomc.domain.Categoria;
 import com.luislopes.cursomc.repositories.CategoriaRepository;
+import com.luislopes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
